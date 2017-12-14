7854da75f675f3f8cf65b6982ff72a8e4b83c778
Salikh Salikhov <k612nn@gmail.com>
2017-12-14 12:10:31 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+console.log("Salikh".toUpperCase());
+var b = a = 5;
+console.log(b+a);
+var test = function(){
+    return 'vadim';
+};
+var func = function(n){
+    console.log('Hello ' + n);
+};
+func(test()); 
+console.log(Math);
+console.log(Math.pow(2,5));
